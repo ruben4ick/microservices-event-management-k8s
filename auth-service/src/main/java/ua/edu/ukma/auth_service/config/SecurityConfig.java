0eb2c4dac48e0a9106f3d8de9701b472c387b264
chucho <bohdan.tarverdiiev@gmail.com>
2025-12-03 19:45:50 +0200

    no metrics

--- auth-service/src/main/java/ua/edu/ukma/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/ua/edu/ukma/auth_service/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ua.edu.ukma.auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,18 +16,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/manage/prometheus").permitAll()
-                        .requestMatchers("/manage/health/**", "/manage/info").permitAll()
-                        .requestMatchers("/manage/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .build();
+                );
+
+        return http.build();
     }
 
     @Bean
